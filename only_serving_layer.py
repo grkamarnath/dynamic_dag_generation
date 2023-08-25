7f3bb6b9228932d5627df1e6059e7fbc1b0ccc8b
amartw <amarnath.gutta@thoughtworks.com>
2023-08-25 21:41:36 +0530

    [Amar] Implemented the only serving layer dag

---
+++ only_serving_layer.py
@@ -0,0 +1,22 @@
+from base_dag import BaseDAG
+
+import airflow
+
+
+class ServingLayer(BaseDAG):
+
+    def make_dependencies(self, dag):
+        self.make_output_port(dag)
+
+
+default_args = {
+    'owner': 'me',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+only_serving = ServingLayer()
+
+# Client Code
+only_serving = only_serving.generate_dag(
+    dag_id='only_serving',
+    args=default_args
+)
